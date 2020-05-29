f07588d68b61ea369b479498d78eda28db8c6f15
christopher <tomiwa_chris@yahoo.com>
2020-05-30 00:21:41 +0100

    i implemented the reset method which helps reset the form after form values have been submitted

--- src/app/apply/apply.component.ts
+++ src/app/apply/apply.component.ts
@@ -1,8 +1,9 @@
 import { Component,ViewChild, AfterViewInit, OnDestroy, OnInit } from '@angular/core';
 import {trigger,style,transition,animate,keyframes,query,stagger,state,} from "@angular/animations";
  import { Content } from '@angular/compiler/src/render3/r3_ast';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, NgForm} from '@angular/forms';
 import { Subscription } from 'rxjs';
+
 
 
 @Component({
@@ -33,20 +34,46 @@ import { Subscription } from 'rxjs';
   
 })
 export class ApplyComponent implements  AfterViewInit, OnDestroy,OnInit {
-  @ViewChild('profileForm') public form: FormGroup;
-  public person = {
-    fullName: '',
+  
+  
+  person: { fullName: string; email: string; github: string; linkedin: string; cv: string; cover: string; };
+  ngOnInit() {
+   // throw new Error("Method not implemented.");
+     this.person= {
+    fullName: "",
     email: '',
     github: '',
     linkedin: '',
-    cover: "",
-};
+    cv: "",
+    cover:""
+    };
+
+
+  }
+  
+  onSubmit(){
+    console.log(this.person);
+    
+    
+  
+    
+  }
+
+ 
+
+
+  @ViewChild('profileForm') public form: FormGroup;
+  
+
+
+
 public value = 0;
     public min = 0;
-    public max = 5;
-    public chunks = 5;
+    public max = 6;
+    public chunks = 6;
 
     public completeness = '0%';
+    
 
     public progressStyle: {[key: string]: any} = {
       background: 'lightgreen'
@@ -62,7 +89,7 @@ public ngAfterViewInit(): void {
   this.formSubscription = this.form.valueChanges.subscribe(x => {
      const completed = Object.keys(this.form.value).reduce((acc, curr) => this.form.value[curr] ? acc + 1 : acc, 0);
      this.value = completed;
-     this.completeness = (completed * 20) + '%';
+     this.completeness = (completed ) + '';
   });
 }
 
@@ -82,6 +109,6 @@ changeState() {
   this.currentState = this.currentState === 'initial' ? 'final': "final"
 }
 
-  
+    
  
 }
